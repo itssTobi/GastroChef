b4816316656d5827560c0d4d09e3850053a336cd
Tobias Eichelberger <211witb04@o365.htl-leoben.at>
2025-09-23 08:59:15 +0200

    setup backend

---
+++ src/backend/at.htlle/src/test/java/gastrochef/at/htlle/GastroChefApplicationTests.java
@@ -0,0 +1,13 @@
+package gastrochef.at.htlle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
