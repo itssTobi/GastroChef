b1bc59ff699700778fe6569b6ae44b3ce67f4a21
Tobias Eichelberger <211witb04@o365.htl-leoben.at>
2025-12-09 08:54:41 +0100

    Add CreateProductRequest DTO class
    Introduces the CreateProductRequest data transfer object with fields for name, description, and pointsCost. Includes Lombok annotations for boilerplate code generation.

---
+++ chefpoints-system/backend/src/main/java/com/gastrochef/dto/CreateProductRequest.java
@@ -0,0 +1,15 @@
+package com.gastrochef.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateProductRequest {
+    private String name;
+    private String description;
+    private Integer pointsCost;
+}
+
